3c39c8362d0abd50ffccf0c29142e06cd2aa2199
Aj052001 <ajsingh052001@gmail.com>
2022-11-15 22:34:34 +0530

    initial commit

--- node.js
+++ node.js
@@ -1,4 +1,19 @@
-const fs = require("fs");
-data = fs.readFileSync("ajay.txt");
-const org = data.toString();
-console.log(org);
+// const fs = require("fs");
+// data = fs.readFileSync("ajay.txt");
+// const org = data.toString();
+// console.log(org);
+let arr =[5,2,6,4];
+for(let i= 0;i<arr.length;i++)
+{
+    for(let j = i+1;j<arr.length;j++)
+    {
+        if(arr[i]>arr[j])
+        {
+            let temp = arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
+   
+}
+console.log(arr)
