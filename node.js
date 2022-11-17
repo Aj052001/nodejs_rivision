7118a16f8e6fdc535365c1de7bcc45f8930bb6d4
Aj052001 <ajsingh052001@gmail.com>
2022-11-17 22:44:13 +0530

    happy birthday add

--- node.js
+++ node.js
@@ -1,48 +1,50 @@
-// const fs = require("fs");
-// data = fs.readFileSync("ajay.txt");
-// const org = data.toString();
-// console.log(org);
-// let arr = [5, 2, 6, 4];
-// for (let i = 0; i < arr.length; i++) {
-//     for (let j = i + 1; j < arr.length; j++) {
-//         if (arr[i] > arr[j]) {
-//             let temp = arr[i];
-//             arr[i] = arr[j];
-//             arr[j] = temp;
-//         }
-//     }
+// // const fs = require("fs");
+// // data = fs.readFileSync("ajay.txt");
+// // const org = data.toString();
+// // console.log(org);
+// // let arr = [5, 2, 6, 4];
+// // for (let i = 0; i < arr.length; i++) {
+// //     for (let j = i + 1; j < arr.length; j++) {
+// //         if (arr[i] > arr[j]) {
+// //             let temapp = arr[i];
+// //             arr[i] = arr[j];
+// //             arr[j] = temapp;
+// //         }
+// //     }
 
-// }
-// console.log(arr)
-
-
+// // }
+// // console.log(arr)
 
 
 
-// let x = 0;
-// do{
-//     console.log(x)
-// }
-// while(x!=0)
 
-// function add()
+
+// // let x = 0;
+// // do{
+// //     console.log(x)
+// // }
+// // while(x!=0)
+
+// // function add()
+// // {
+// //     console.log("Happy BirthDay Ankita");
+// // }
+// // add();
+
+// //array
+// let arr =[3,2,4,6,4,1];
+// for(let i = 0; i<arr.length;i++)
 // {
-//     console.log("Happy BirthDay Ankita");
+//   for(let j = i+1 ;j<arr.length;j++)
+//   {
+//     if(arr[i]>arr[j])
+//     {
+//         let temapp = arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=temapp;
+//     }
+//   }
 // }
-// add();
+// console.log(...arr)
 
-//array
-let arr =[3,2,4,6,4,1];
-for(let i = 0; i<arr.length;i++)
-{
-  for(let j = i+1 ;j<arr.length;j++)
-  {
-    if(arr[i]>arr[j])
-    {
-        let temp = arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
-  }
-}
-console.log(...arr)
+
---
+++ wp_msg.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+919079676928','Happy BirthDay Ajay aisn',22,43 )
